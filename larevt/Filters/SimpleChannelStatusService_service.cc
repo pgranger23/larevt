9e64bc700d0e9fb81ee61f8167503367d2476c17
Chris Green <greenc@fnal.gov>
2022-04-05 11:23:04 -0500

    Resolve remaining CMake warnings

--- larevt/Filters/SimpleChannelStatusService_service.cc
+++ larevt/Filters/SimpleChannelStatusService_service.cc
@@ -1,6 +1,6 @@
 /**
  * @file   SimpleChannelStatusService_service.cc
- * @brief  Service for channel quality info
+ * @brief  Service Registration for channel quality info
  * @author Gianluca Petrillo (petrillo@fnal.gov)
  * @date   November 24th, 2014
  * @see    SimpleChannelStatusService.h
@@ -9,40 +9,12 @@
 // Our header
 #include "larevt/Filters/SimpleChannelStatusService.h"
 
-// LArSoft libraries
-#include "larcore/Geometry/Geometry.h"
-
 // Framework libraries
 #include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "messagefacility/MessageLogger/MessageLogger.h"
 
 namespace lariov {
 
-  //----------------------------------------------------------------------------
-  SimpleChannelStatusService::SimpleChannelStatusService(fhicl::ParameterSet const& pset)
-  {
-    SimpleChannelStatus* simple_filter = new SimpleChannelStatus(pset);
-
-    raw::ChannelID_t MaxChannel
-      = raw::ChannelID_t(art::ServiceHandle<geo::Geometry const>()->Nchannels() - 1);
-
-    simple_filter->Setup(MaxChannel);
-
-    fProvider.reset(simple_filter);
-
-    mf::LogInfo("SimpleChannelStatusService") << "Loaded from configuration:"
-      << "\n  - " << fProvider->BadChannels().size() << " bad channels"
-      << "\n  - " << fProvider->NoisyChannels().size() << " noisy channels"
-      << "\n  - largest channel ID: " << fProvider->MaxChannel()
-        << ", largest present: " << fProvider->MaxChannelPresent()
-      ;
-
-  } // SimpleChannelStatusService::SimpleChannelStatusService()
-
-
-  //----------------------------------------------------------------------------
   DEFINE_ART_SERVICE_INTERFACE_IMPL
     (lariov::SimpleChannelStatusService, lariov::ChannelStatusService)
 
-} // namespace filter
+}
